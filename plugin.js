9f4613a6ae87059c64ed832ca5b687a6ed29c944
Apocist <apocist@yahoo.com>
2016-04-22 16:53:27 -0700

    Initial Commit

---
+++ plugin.js
@@ -0,0 +1,17 @@
+module.exports = function(callback) {
+	callback({
+		name: 'Twitter Authentication',
+		description: 'Allows users via twitter',
+		models: {},
+		controllers: {},
+		routes: {},
+		views: {},
+		loadOrder:{
+			'passport.strategy': [{
+				order: 2000,//doesn't really matter
+				item: __dirname+'/app/controller/auth/twitter.auth.server.strategy.js'
+			}]
+		},
+		defaults: require(__dirname+'/config/defaults.json')
+	});
+};
